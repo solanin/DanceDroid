777b022db25f103b51962b3ed70410115f504af5
Ashley Ervin <aee9149@rit.edu>
2015-11-09 13:51:36 -0500

    made button list

--- app/src/main/java/com/ash/ervin/dancedroid/MyActivity.java
+++ app/src/main/java/com/ash/ervin/dancedroid/MyActivity.java
@@ -2,12 +2,47 @@ package com.ash.ervin.dancedroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MyActivity extends AppCompatActivity {
+
+    //
+    ArrayList<Button> buttonList = new ArrayList<>();
+    Button one;
+    Button two;
+    Button three;
+    Button four;
+    Button five;
+    Button six;
+    Button seven;
+    Button eight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        addButtons();
+
+    }
+    //creates buttons and puts them in the button list
+    //in a slow way that should probably be optimized somehow
+    private void addButtons(){
+        one = (Button)findViewById(R.id.button_one);
+        two = (Button) findViewById(R.id.button_two);
+        three = (Button) findViewById(R.id.button_three);
+        four = (Button) findViewById(R.id.button_four);
+        five = (Button) findViewById(R.id.button_five);
+        six = (Button) findViewById(R.id.button_six);
+        seven = (Button) findViewById(R.id.button_seven);
+
+        buttonList.add(one);
+        buttonList.add(two);
+        buttonList.add(three);
+        buttonList.add(four);
+        buttonList.add(five);
+        buttonList.add(six);
+        buttonList.add(seven);
     }
 }
